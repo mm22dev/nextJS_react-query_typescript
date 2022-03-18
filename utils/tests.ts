c67509736771d228f025c643bc11d752cd192e78
mm_redev <mirkomonaco89@gmail.com>
2022-03-18 22:08:51 +0100

    chore(config): add test config

---
+++ utils/tests.ts
@@ -0,0 +1,23 @@
+// jest-dom adds custom jest matchers for asserting on DOM nodes.
+// allows you to do things like:
+// expect(element).toHaveTextContent(/react/i)
+// learn more: https://github.com/testing-library/jest-dom
+import '@testing-library/jest-dom'
+import { setupServer } from 'msw/node'
+import { rest } from 'msw'
+import { setLogger } from 'react-query'
+
+const server = setupServer()
+
+beforeAll(() => server.listen())
+afterEach(() => server.resetHandlers())
+afterAll(() => server.close())
+
+// silence react-query errors
+setLogger({
+  log: console.log,
+  warn: console.warn,
+  error: () => {},
+})
+
+export { server, rest }
